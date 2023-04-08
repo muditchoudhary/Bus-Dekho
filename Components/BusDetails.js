bbde140732a2d23123b815ae5573904991935b06
Mudit Choudhary <cmudit60@gmail.com>
2023-04-08 16:10:41 +0530

    Update Busdetails.js

--- Components/BusDetails.js
+++ Components/BusDetails.js
@@ -1,13 +1,33 @@
 import * as React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
 import cardImg from "../assets/card-demo-img.png";
 
 function BusDetails() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Bus Details Screen</Text>
-    </View>
+   <ScrollView style={styles.scrollViewContainer}>
+        <View style={styles.locationsMainContainer}>
+            <Text>Via Locations</Text>
+            <View style={styles.locationsSubContainer}>
+                <Text>Locations to show</Text>
+            </View>
+        </View>
+   </ScrollView>
   );
 }
 
+const styles = StyleSheet.create({
+    scrollViewContainer: {
+        flex: 1,
+        borderWidth: 2,
+        borderColor: "red"
+    },
+    locationsMainContainer: {
+        borderWidth: 2,
+        borderColor: "green"
+    },
+    locationsSubContainer: {
+        borderWidth: 2,
+        borderColor: "blue"
+    }
+})
 export default BusDetails;
